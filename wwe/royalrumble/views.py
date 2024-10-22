8e8efcf075c242a9bdb76d4573c7dedca6da4a5e
ngelw <ngelw>
2024-10-22 12:18:24 -0500

    creation of upload.html templates

--- wwe/royalrumble/views.py
+++ wwe/royalrumble/views.py
@@ -3,36 +3,7 @@ from bs4 import BeautifulSoup
 from django.shortcuts import render
 
 # Create your views here.
-def scrape_wwe_royal_rumble_2009():
-    url = "https://www.wwe.com/superstars"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # This will find all the superstar containers (adjust if needed)
-        superstar_cards = soup.find_all('img')
-
-        players = []
-        for card in superstar_cards:
-            # Assuming the name is in a tag (like an h2) within the card
-            name_tag = card.find('h2')  # Adjust based on actual structure
-            image_tag = card.find('img')  # Find the img tag for the wrestler
-
-            if name_tag and image_tag:
-                player_info = {
-                    'name': name_tag.get_text(strip=True),
-                    'image_url': image_tag['src']  # Get the src attribute of the img tag
-                }
-                players.append(player_info)
-
-        return players
-    else:
-        return []
-
-def home(request):
-    # Scrape the player names and images
-    players = scrape_wwe_royal_rumble_2009()
-
-    # Pass the players to the template
-    return render(request, "royalrumble/index.html", {"players": players})
+def home(requests):
+    return render(requests, 'royalrumble/index.html')
+def upload(requests):
+    return render(requests, 'royalrumble/upload.html')
--- wwe/wwe/urls.py
+++ wwe/wwe/urls.py
@@ -20,5 +20,6 @@ from royalrumble.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home)
+    path('',home),
+    path('upload/',upload)
 ]
